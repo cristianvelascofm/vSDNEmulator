d8a5b3d5cc3bc27b200db637dcff899ba7c3f7e1
cristianvelascofm <cfvelasco@unicauca.edu.co>
2020-11-23 21:15:45 -0500

    Pruenas Cliente - Server Mininet

--- AEmulator/Tesis/static/client/client.py
+++ AEmulator/Tesis/static/client/client.py
@@ -3,25 +3,28 @@ import sys
 import json
 
 
-
-
-
 def load_data(d):
     datatr = d
     
     # Convierte en JSON el objeto python
     filejson = json.dumps(datatr)
     print("Resutlado Final : ", filejson)
-    
+
+    # Conexión con wireshark a través de Xming
+      
 
 
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #sock_w = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     # Connect the socket to the port where the server is listening
-    server_address = ('127.0.0.1', 10000)
+    server_address = ('192.168.56.103', 10000)
+    #server_address_w = ('192.168.56.103', 11000)
     print('connecting to {} port {}'.format(*server_address))
+    #print('connecting to {} port {}'.format(*server_address_w))
     sock.connect(server_address)
+    #sock_w.connect(server_address_w)
 
     try:
         # Send data
